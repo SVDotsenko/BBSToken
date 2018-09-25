76c6df070f0f1170145d94e83e76c704e99eb1c5
Serhii Dotsenko <serdotsenko@gmail.com>
2018-09-25 18:15:59 +0300

    transfer works without metamask

--- main.js
+++ main.js
@@ -374,8 +374,6 @@ var contractABI =
   ]
   ;
 
-// const Web3 = require("web3");
-
 function addToRopsten() {
   go(true);
 }
@@ -386,53 +384,24 @@ function getFromRopsten() {
 
 const testNet = 'http://localhost:8545';
 const contractAddress = '0x6c90732441c99b8c5f2d47f2280e1c5a00da89b6';
-const walletAddress1 = '0x5e042fbab85df501dffb0aad30e159d15bb388bd';
-const privateKey1 = 'd3a8ec53b34da61c8e5e00ed5445424421cab147e9ffcc1c649ffa1a0a968ed5'
-const walletAddress2 = '0xE7253fe2834559604dc917Cbe8420301912d0445';
-
-// function sendSigned(txData, cb) {
-//   const privateKey = Buffer.from(privateKey1, 'hex')
-//   const transaction = new Tx(txData)
-//   transaction.sign(privateKey)
-//   const serializedTx = transaction.serialize().toString('hex')
-//   web3.eth.sendSignedTransaction('0x' + serializedTx, cb)
-// }
 
 function go(addOrGet) {
+  const senderWallet = document.getElementById('swallet').value;
+  const recieverWallet = document.getElementById('rwallet').value;
+
   if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
   } else {
     web3 = new Web3(new Web3.providers.HttpProvider(testNet));
   }
+
   console.log(web3.version);
+
   var myContract = new web3.eth.Contract(contractABI, contractAddress);
 
   if (addOrGet) {
-    // console.log('web3.eth.defaultAccount ' + web3.eth.defaultAccount);
-    // var myKey = document.getElementById('addKey').value;
-    // var myAmount = document.getElementById('addAmount').value;
-    web3.eth.defaultAccount = walletAddress1;
-    // console.log('web3.eth.defaultAccount ' + web3.eth.defaultAccount);
-
-    // web3.eth.personal.unlockAccount(walletAddress1, privateKey1, 1000).then(data => console.log(data)).catch(err => console.log(err));
-
-    // web3.eth.getTransactionCount(walletAddress1).then(txCount => {
-    //   const txData = {  // construct the transaction data
-    //     nonce: web3.utils.toHex(txCount),
-    //     gasLimit: web3.utils.toHex(25000),
-    //     gasPrice: web3.utils.toHex(10e9), // 10 Gwei
-    //     to: walletAddress2,
-    //     from: walletAddress1,
-    //     value: web3.utils.toHex(web3.utils.toWei('3', 'wei'))
-    //   }
-    //   // fire away!
-    //   sendSigned(txData, function (err, result) {
-    //     if (err) return console.log('error', err)
-    //     console.log('sent', result)
-    //   })
-    // })
-
-    myContract.methods.burn(1).send({ from: walletAddress1 }, function (error, transactionHash) {//https://web3js.readthedocs.io/en/1.0/web3-eth-contract.html#methods-mymethod-send
+    web3.eth.defaultAccount = senderWallet;
+    myContract.methods.burn(1).send({ from: senderWallet }, function (error, transactionHash) {
       if (error) {
         console.log("err happend: " + error);
       } else {
@@ -440,35 +409,32 @@ function go(addOrGet) {
       }
     });
 
-  } else {//https://web3js.readthedocs.io/en/1.0/web3-eth-contract.html#methods-mymethod-call
-    myContract.methods.balanceOf(walletAddress1).call({ from: walletAddress1 }, function (error, result) {
-      console.log("balance of the wallet " + walletAddress1 + " is " + result);
+  } else {
+    myContract.methods.balanceOf(senderWallet).call({ from: senderWallet }, function (error, result) {
+      console.log("balance of the wallet " + senderWallet + " is " + result);
     });
-    myContract.methods.balanceOf(walletAddress2).call({ from: walletAddress2 }, function (error, result) {
-      console.log("balance of the wallet " + walletAddress2 + " is " + result);
+    myContract.methods.balanceOf(recieverWallet).call({ from: recieverWallet }, function (error, result) {
+      console.log("balance of the wallet " + recieverWallet + " is " + result);
     });
   }
 }
 
-function sendFromTo() {
+function transferMetamask() {
+  const senderWallet = document.getElementById('swallet').value;
+  const recieverWallet = document.getElementById('rwallet').value;
+  const amount = document.getElementById('amount').value;
+
   if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
   } else {
     web3 = new Web3(new Web3.providers.HttpProvider(testNet));
   }
+
   console.log(web3.version);
   var myContract = new web3.eth.Contract(contractABI, contractAddress);
-  web3.eth.defaultAccount = walletAddress1;
+  web3.eth.defaultAccount = senderWallet;
 
-  // myContract.methods.transferFrom(walletAddress1, walletAddress2, 1).send({ from: walletAddress1 }, function (error, transactionHash) {
-  //   if (error) {
-  //     console.log("err happend: " + error);
-  //   } else {
-  //     console.log("transaction Hash: " + transactionHash);
-  //   }
-  // });
-
-  myContract.methods.transfer(walletAddress2, 1).send({ from: walletAddress1 }, function (error, transactionHash) {
+  myContract.methods.transfer(recieverWallet, amount).send({ from: senderWallet }, function (error, transactionHash) {
     if (error) {
       console.log("err happend: " + error);
     } else {
@@ -482,4 +448,34 @@ function createWallet() {//https://web3js.readthedocs.io/en/1.0/web3-eth-account
   var newWalletAdress = web3.eth.accounts.wallet.create(1);
   console.log("the new Wallet Adress is " + newWalletAdress[0].address);
   console.log("the private key from that adress is " + newWalletAdress[0].privateKey);
+}
+
+function transfer() {  
+  const senderWallet = document.getElementById('swallet').value;
+  const recieverWallet = document.getElementById('rwallet').value;
+  const amount = document.getElementById('amount').value;
+
+  var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/22b273651d424974b7ef0de70a7ed880'));  
+  web3.eth.getTransactionCount(senderWallet, function (err, nonce) {
+    var myContract = new web3.eth.Contract(contractABI, contractAddress);
+    var tx = new ethereumjs.Tx({
+      nonce: nonce,
+      gasPrice: web3.utils.toHex(web3.utils.toWei('50', 'gwei')),
+      gasLimit: 100000,
+      to: contractAddress,
+      value: 0,
+      data: myContract.methods.transfer(recieverWallet, amount).encodeABI(),
+    });
+    
+    const senderKey = 'd3a8ec53b34da61c8e5e00ed5445424421cab147e9ffcc1c649ffa1a0a968ed5';
+    tx.sign(ethereumjs.Buffer.Buffer.from(senderKey, 'hex'));
+
+    web3.eth.sendSignedTransaction('0x' + tx.serialize().toString('hex'), function (err, transactionHash) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("transaction Hash: " + transactionHash);
+      }
+    });
+  });
 }
